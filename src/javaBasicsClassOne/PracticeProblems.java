fb1f4842163b6472947f9e608f57e96a7c240c3f
sahil <sahilhans0605@gmail.com>
2022-05-31 15:12:20 +0530

    Java + DSA by Sahil hans

---
+++ src/javaBasicsClassOne/PracticeProblems.java
@@ -0,0 +1,13 @@
+//1.Write a Java program that takes two numbers as input and display the product of two numbers.
+//
+//        2.Write a Java program to print the area and perimeter of a circle.
+//
+//        3.Write a Java program that reads a number in inches,converts it to meters.
+//
+//        4.Take three numbers from the user and print the greatest number.
+//
+//        5.Write a Java program that takes a year from user and print whether that year is a leap year or not.
+//
+//        6.Write a Java program to swap two numbers.
+//
+//        7.Write a Java program to grade students based on their marks.
